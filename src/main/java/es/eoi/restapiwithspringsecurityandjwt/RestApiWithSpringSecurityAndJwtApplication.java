a7de855d73e3809290d6b614d953f85cdbc4a33f
Jose Francisco <josefranciscomunozespinosa@gmail.com>
2021-08-19 20:18:12 +0200

    Spring initializr
    Esqueleto del proyecto creado a partir de la web de Spring initializr http://start.spring.io/
    
    - web
    - security
    - jpa
    - lombok

---
+++ src/main/java/es/eoi/restapiwithspringsecurityandjwt/RestApiWithSpringSecurityAndJwtApplication.java
@@ -0,0 +1,13 @@
+package es.eoi.restapiwithspringsecurityandjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithSpringSecurityAndJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiWithSpringSecurityAndJwtApplication.class, args);
+	}
+
+}
